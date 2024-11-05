f79df986a3b8296eb751b522fa2a996e4ad0a6ec
ynivermine92 <ynivermine92@gmail.com>
2024-11-05 19:59:19 +0200

    custimers

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+
+const bureger = document.querySelector('.customers__burger');
+const customers = document.querySelector('.customers');
+const sidebar = document.querySelector('.sidebar');
+const clouse = document.querySelector('.sidebar__clouse');
+const sidebarItems = document.querySelectorAll('.sidebar__items');
+
+
+
+const burgerMenu = () => {
+    bureger.addEventListener('click', () => {
+        customers.classList.remove('active');
+        sidebar.classList.add('active');
+    })
+
+    clouse.addEventListener('click', () => {
+        sidebar.classList.remove('active');
+        customers.classList.add('active');
+    })
+}
+burgerMenu();
+
+
+
+sidebarItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        sidebar.classList.remove('active');
+        customers.classList.add('active');
+    })
+})
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,2 @@
-// Custom Scripts
 
-/* @@include('./libs//fancybox(lib).js')  */
-@@include('./libs//header.js')
 @@include('main.js')
